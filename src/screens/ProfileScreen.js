1bc2e3759ee556aa60a5767b5794ca345e36c37d
unknown <haseebnk37@gmail.com>
2022-04-08 16:27:26 +0500

    nw changes

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,46 +1,46 @@
-import  React , {useContext} from "react";
-import {View,Text , FlatList , Button , StyleSheet} from "react-native";
+import React, { useContext } from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { NotesContext } from "../context/NotesContext";
 
 
-export default function ProfileScreen({ navigation , }) {
+export default function ProfileScreen({ navigation, }) {
 
     // const { id } = route.params
-    const { state  } = useContext(NotesContext)
+    const { state } = useContext(NotesContext)
 
-    const note = state.find((record) => {
-        return record.id === id
-    })
+    // const note = state.find((record) => {
+    //     return record.id === id
+    // })
 
 
 
 
     return (
         <View>
-           <FlatList
+            <FlatList
                 data={state}
                 keyExtractor={item => item.name}
                 renderItem={({ item }) => {
                     return (
-                        
+
 
                         <View >
 
-                          {console.log(item)}  
-                       
+                            {console.log(item)}
 
-                            <LinearGradient   style={styles.withBorder}  colors={[item.color[0], item.color[1]] }  title="Welcome">
 
- <Text style={{alignSelf:'center', fontSize: 22  , color:'white' ,}} >{item.name}</Text>
-                        <Text style={{ alignSelf:'center', fontSize: 22  , color:'white' ,}} >{item.number}</Text>
+                            <LinearGradient style={styles.withBorder} colors={[item.color[0], item.color[1]]} title="Welcome">
+
+                                <Text style={{ alignSelf: 'center', fontSize: 22, color: 'white', }} >{item.name}</Text>
+                                <Text style={{ alignSelf: 'center', fontSize: 22, color: 'white', }} >{item.number}</Text>
 
                             </LinearGradient>
-                     
-                     
-                      </View>
-                      
-                       
+
+
+                        </View>
+
+
                     )
                 }}
             />
@@ -48,7 +48,7 @@ export default function ProfileScreen({ navigation , }) {
     )
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
     withOutBorder:
     {
         marginHorizontal: 4,
@@ -60,26 +60,26 @@ const styles= StyleSheet.create({
         color: "White",
     }
     ,
-  
+
     withBorder:
     {
         marginHorizontal: 4,
         borderRadius: 10,
-      marginVertical:10,
-    
-        width:'90%',
+        marginVertical: 10,
+
+        width: '90%',
         height: 158,
-        alignSelf:'center',
-       
+        alignSelf: 'center',
+
     },
-  
-  input:{
-    height:50,
-    alignSelf:"center",
-    backgroundColor:'white' ,
-    width:'80%',
-    margin:10,
-    
-  }
-  
-  })
+
+    input: {
+        height: 50,
+        alignSelf: "center",
+        backgroundColor: 'white',
+        width: '80%',
+        margin: 10,
+
+    }
+
+})
